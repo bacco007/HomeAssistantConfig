ab1fa1235723961451a140b551a08c7af1c4d7fc
Thomas Baxter <thomas@thomasbaxter.info>
2019-12-30 17:36:05 +1100

    More Lovelace additions

--- www/custom-lovelace/development/sports-upnext-card.js
+++ www/custom-lovelace/development/sports-upnext-card.js
@@ -1,15 +1,15 @@
-const fireEvent = (node, type, detail, options) => {
-  options = options || {};
-  detail = detail === null || detail === undefined ? {} : detail;
-  const event = new Event(type, {
-    bubbles: options.bubbles === undefined ? true : options.bubbles,
-    cancelable: Boolean(options.cancelable),
-    composed: options.composed === undefined ? true : options.composed
-  });
-  event.detail = detail;
-  node.dispatchEvent(event);
-  return event;
-};
+// const fireEvent = (node, type, detail, options) => {
+//   options = options || {};
+//   detail = detail === null || detail === undefined ? {} : detail;
+//   const event = new Event(type, {
+//     bubbles: options.bubbles === undefined ? true : options.bubbles,
+//     cancelable: Boolean(options.cancelable),
+//     composed: options.composed === undefined ? true : options.composed
+//   });
+//   event.detail = detail;
+//   node.dispatchEvent(event);
+//   return event;
+// };
 
 class SportsUpNextCard extends HTMLElement {
   constructor() {
