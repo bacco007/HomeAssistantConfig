4410a04808fd7261ce8e2a032a0d40edf0a4312a
root <root@homeassistant.local.hass.io>
2022-04-16 16:35:15 +1000

    Automated: HA Config as at 16/04/2022 16:35

--- bin/ha_gitpush.sh
+++ bin/ha_gitpush.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git add . >/dev/null
+git add . >> /dev/null
 
 NOW=$(date +"%d/%m/%Y %H:%M")
 
-git commit -m "Automated: HA Config as at $NOW" >/dev/null
+git commit -m "Automated: HA Config as at $NOW" >> /dev/null
 
-git push -u origin master >/dev/null
+git push -u origin master >> /dev/null
