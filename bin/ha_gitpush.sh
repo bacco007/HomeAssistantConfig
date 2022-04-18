7db9376ea63d747a4aa02a35cc4ed2ec2454544c
Thomas Baxter <thomas@thomasbaxter.info>
2022-04-18 15:30:43 +1000

    Automated: HA Config as at 18/04/2022 15:30

--- bin/ha_gitpush.sh
+++ bin/ha_gitpush.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ssh-keyscan github.com >> ~/.ssh/known_hosts > file.log
+ssh-keyscan github.com >> ~/.ssh/known_hosts > file.log 2>&1
 
-git config user.name "Thomas Baxter" > file.log
+git config user.name "Thomas Baxter" > file.log 2>&1
 
-git config user.email "thomas@thomasbaxter.info" > file.log
+git config user.email "thomas@thomasbaxter.info" > file.log 2>&1
 
-git add . > file.log
+git add . > file.log 2>&1
 
-NOW=$(date +"%d/%m/%Y %H:%M") > file.log
+NOW=$(date +"%d/%m/%Y %H:%M") > file.log 2>&1
 
-git commit -m "Automated: HA Config as at $NOW" > file.log
+git commit -m "Automated: HA Config as at $NOW" > file.log 2>&1
 
-git push -u origin master > file.log
+git push -u origin master > file.log 2>&1
