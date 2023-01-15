7d101a565909ef98f14ec87c3b359f13214a6d03
Thomas Baxter <thomas@thomasbaxter.info>
2023-01-16 09:00:46 +1100

    Update Dep: custom_components/astroweather via HACS

--- custom_components/astroweather/sensor.py
+++ custom_components/astroweather/sensor.py
@@ -7,14 +7,15 @@
     Author: Markus Winkler
 """
 import logging
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.util import dt as dt_util
+
 from homeassistant.const import (
-    DEVICE_CLASS_HUMIDITY,
-    DEVICE_CLASS_TEMPERATURE,
-    DEVICE_CLASS_TIMESTAMP,
     DEGREE,
     LENGTH_METERS,
     PERCENTAGE,
@@ -71,7 +72,7 @@ SENSOR_TYPES = {
         "Timestamp",
         "",
         "mdi:calendar",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "cloudcover_percentage": [
@@ -127,7 +128,7 @@ SENSOR_TYPES = {
         "Lifted Index",
         TEMP_CELSIUS,
         "mdi:arrow-expand-up",
-        DEVICE_CLASS_TEMPERATURE,
+        SensorDeviceClass.TEMPERATURE,
         STATE_CLASS_MEASUREMENT,
     ],
     "lifted_index_plain": [
@@ -141,7 +142,7 @@ SENSOR_TYPES = {
         "2m Relative Humidity",
         PERCENTAGE,
         "mdi:water-percent",
-        DEVICE_CLASS_HUMIDITY,
+        SensorDeviceClass.HUMIDITY,
         STATE_CLASS_MEASUREMENT,
     ],
     "wind10m_direction": [
@@ -169,14 +170,14 @@ SENSOR_TYPES = {
         "2m Temperature",
         TEMP_CELSIUS,
         "mdi:thermometer",
-        DEVICE_CLASS_TEMPERATURE,
+        SensorDeviceClass.TEMPERATURE,
         STATE_CLASS_MEASUREMENT,
     ],
     "dewpoint2m": [
         "2m Dewpoint",
         TEMP_CELSIUS,
         "mdi:thermometer",
-        DEVICE_CLASS_TEMPERATURE,
+        SensorDeviceClass.TEMPERATURE,
         STATE_CLASS_MEASUREMENT,
     ],
     "prec_type": [
@@ -211,56 +212,56 @@ SENSOR_TYPES = {
         "Sun Next Setting",
         "",
         "mdi:weather-sunset-down",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "sun_next_setting_nautical": [
         "Sun Next Setting Nautical",
         "",
         "mdi:weather-sunset-down",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "sun_next_setting_astro": [
         "Sun Next Setting Astronomical",
         "",
         "mdi:weather-sunset-down",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "sun_next_rising": [
         "Sun Next Rising",
         "",
         "mdi:weather-sunset-down",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "sun_next_rising_nautical": [
         "Sun Next Rising Nautical",
         "",
         "mdi:weather-sunset-down",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "sun_next_rising_astro": [
         "Sun Next Rising Astronomical",
         "",
         "mdi:weather-sunset-down",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "moon_next_rising": [
         "Moon Next Rising",
         "",
         "mdi:arrow-up-circle-outline",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "moon_next_setting": [
         "Moon Next Setting",
         "",
         "mdi:arrow-down-circle-outline",
-        DEVICE_CLASS_TIMESTAMP,
+        SensorDeviceClass.TIMESTAMP,
         None,
     ],
     "moon_phase": [
@@ -375,7 +376,10 @@ class AstroWeatherSensor(AstroWeatherEntity, SensorEntity):
     @property
     def native_value(self):
         """Return the state of the sensor."""
-        if SENSOR_TYPES[self._sensor][SENSOR_DEVICE_CLASS] == DEVICE_CLASS_TIMESTAMP:
+        if (
+            SENSOR_TYPES[self._sensor][SENSOR_DEVICE_CLASS]
+            == SensorDeviceClass.TIMESTAMP
+        ):
             return dt_util.parse_datetime(
                 str(getattr(self.coordinator.data[SENSOR_NAME], self._sensor, None))
             )
@@ -385,7 +389,10 @@ class AstroWeatherSensor(AstroWeatherEntity, SensorEntity):
     @property
     def native_unit_of_measurement(self):
         """Return the unit of measurement."""
-        if SENSOR_TYPES[self._sensor][SENSOR_DEVICE_CLASS] == DEVICE_CLASS_TIMESTAMP:
+        if (
+            SENSOR_TYPES[self._sensor][SENSOR_DEVICE_CLASS]
+            == SensorDeviceClass.TIMESTAMP
+        ):
             return None
         else:
             return SENSOR_TYPES[self._sensor][SENSOR_UNIT]
