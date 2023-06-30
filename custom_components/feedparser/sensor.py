9ae4e89494944235a1b9050ca15d96c85cf9ebb2
Thomas Baxter <thomas@thomasbaxter.info>
2023-06-30 18:00:35 +1000

    Update Dep: custom_components/feedparser via HACS

--- custom_components/feedparser/sensor.py
+++ custom_components/feedparser/sensor.py
@@ -17,7 +17,7 @@ import homeassistant.util.dt as dt
 
 import feedparser
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 
 COMPONENT_REPO = "https://github.com/custom-components/sensor.feedparser/"
 
