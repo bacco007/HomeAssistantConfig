6e4138d84e1e460900ceb59a250a0892b253bbb8
Thomas Baxter <thomas@thomasbaxter.info>
2023-07-24 17:44:16 +1000

    .gitignore update

--- bin/ha_gitpush.sh
+++ bin/ha_gitpush.sh
@@ -7,9 +7,16 @@ git config user.name "Thomas Baxter" > file.log 2>&1
 git config user.email "thomas@thomasbaxter.info" > file.log 2>&1
 
 git add . > file.log 2>&1
+echo "-----> git add done"
+
+git status
+echo "-----> git status done"
 
 NOW=$(date +"%d/%m/%Y %H:%M") > file.log 2>&1
 
 git commit -m "Automated: HA Config as at $NOW" > file.log 2>&1
+echo "-----> git commit done"
 
 git push -u origin master > file.log 2>&1
+echo "-----> git push done"
+echo "-----> all done"
