48cd84d051e7a4b10c0aa5fd735ad8df43cbdacc
Thomas Baxter <thomas@thomasbaxter.info>
2023-12-21 12:00:15 +1100

    Update Dep: custom_components/flightradar24 via HACS

--- custom_components/flightradar24/config_flow.py
+++ custom_components/flightradar24/config_flow.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import voluptuous as vol
 from typing import Any
 from homeassistant import config_entries
@@ -7,6 +8,7 @@ from .const import (
 )
 import homeassistant.helpers.config_validation as cv
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.core import callback
 from homeassistant.const import (
     CONF_LATITUDE,
     CONF_LONGITUDE,
@@ -35,4 +37,31 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 CONF_LONGITUDE: self.hass.config.longitude,
             },
         )
-                                    )
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: config_entries.ConfigEntry) -> config_entries.OptionsFlow:
+        """Return the options flow."""
+        return OptionsFlow(config_entry)
+
+
+class OptionsFlow(config_entries.OptionsFlow):
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None) -> FlowResult:
+        if user_input is not None:
+            self.config_entry.data = user_input
+            return self.async_create_entry(title=DEFAULT_NAME, data=user_input)
+
+        data = self.config_entry.data
+
+        data_schema = vol.Schema({
+                vol.Required(CONF_RADIUS, default=data.get(CONF_RADIUS)): vol.Coerce(float),
+                vol.Required(CONF_LATITUDE, default=data.get(CONF_LATITUDE)): cv.latitude,
+                vol.Required(CONF_LONGITUDE, default=data.get(CONF_LONGITUDE)): cv.longitude,
+                vol.Required(CONF_SCAN_INTERVAL, default=data.get(CONF_SCAN_INTERVAL)): int,
+            })
+
+        return self.async_show_form(step_id="init", data_schema=data_schema)
