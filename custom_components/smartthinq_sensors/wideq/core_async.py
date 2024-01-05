e7ce1c5c1db17097545d71ad3976d1df13da12a0
Thomas Baxter <thomas@thomasbaxter.info>
2024-01-05 18:00:16 +1100

    Update Dep: custom_components/smartthinq_sensors via HACS

--- custom_components/smartthinq_sensors/wideq/core_async.py
+++ custom_components/smartthinq_sensors/wideq/core_async.py
@@ -104,7 +104,12 @@ _LG_SSL_CIPHERS = (
     "DEFAULT:!aNULL:!eNULL:!MD5:!3DES:!DES:!RC4:!IDEA:!SEED:!aDSS:!SRP:!PSK"
 )
 
+_COMMON_LANG_URI_ID = "langPackCommonUri"
 _LOCAL_LANG_FILE = "local_lang_pack.json"
+
+_HOME_ID = "homeId"
+_HOME_NAME = "homeName"
+_HOME_CURRENT = "currentHomeYn"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -170,6 +175,7 @@ class CoreAsync:
         self._language = language
         self._timeout = aiohttp.ClientTimeout(total=timeout)
         self._oauth_url = oauth_url
+        self._lang_pack_url = None
 
         if session:
             self._session = session
@@ -187,6 +193,11 @@ class CoreAsync:
     def language(self):
         """Return the used language."""
         return self._language
+
+    @property
+    def lang_pack_url(self):
+        """Return the used language."""
+        return self._lang_pack_url
 
     async def close(self):
         """Close the managed session on exit."""
@@ -401,6 +412,8 @@ class CoreAsync:
         gateway_result = self._manage_lge_result(out)
         _LOGGER.debug("Gateway info: %s", gateway_result)
         self._oauth_url = gateway_result["oauthUri"]
+        if self._lang_pack_url is None and _COMMON_LANG_URI_ID in gateway_result:
+            self._lang_pack_url = gateway_result[_COMMON_LANG_URI_ID]
         return self._oauth_url
 
     async def gateway_info(self):
@@ -990,6 +1003,7 @@ class Session:
         """Initialize session object."""
         self._auth = auth
         self.session_id = session_id
+        self._homes: dict | None = None
         self._common_lang_pack_url = None
 
     @property
@@ -1055,14 +1069,86 @@ class Session:
             self._auth.user_number,
         )
 
-    async def get_devices(self) -> list[dict]:
+    async def _get_homes(self) -> dict | None:
+        """Get a dict of homes associated with the user's account."""
+        if self._homes is not None:
+            return self._homes
+
+        homes = await self.get2("service/homes")
+        if not isinstance(homes, dict):
+            _LOGGER.warning("LG API return invalid homes information: '%s'", homes)
+            return None
+
+        _LOGGER.debug("Received homes: %s", homes)
+        loaded_homes = {}
+        homes_list = as_list(homes.get("item", []))
+        for home in homes_list:
+            if home_id := home.get(_HOME_ID):
+                loaded_homes[home_id] = {
+                    _HOME_NAME: home.get(_HOME_NAME, "unamed home"),
+                    _HOME_CURRENT: home.get(_HOME_CURRENT, "N"),
+                }
+
+        if loaded_homes:
+            self._homes = loaded_homes
+        return loaded_homes
+
+    async def _get_home_devices(self, home_id: str) -> list[dict] | None:
+        """
+        Get a list of devices associated with the user's home_id.
+        Return information about the devices.
+        """
+        dashboard = await self.get2(f"service/homes/{home_id}")
+        if not isinstance(dashboard, dict):
+            _LOGGER.warning(
+                "LG API return invalid devices information for home_id %s: '%s'",
+                home_id,
+                dashboard,
+            )
+            return None
+
+        if self._common_lang_pack_url is None:
+            if _COMMON_LANG_URI_ID in dashboard:
+                self._common_lang_pack_url = dashboard[_COMMON_LANG_URI_ID]
+            else:
+                self._common_lang_pack_url = self._auth.gateway.core.lang_pack_url
+        return as_list(dashboard.get("devices", []))
+
+    async def get_devices(self) -> list[dict] | None:
         """
         Get a list of devices associated with the user's account.
         Return information about the devices.
         """
+        if not (homes := await self._get_homes()):
+            _LOGGER.warning("Not possible to determinate a valid home_id")
+            return None
+
+        valid_home = False
+        devices_list = []
+        for home_id in homes:
+            if (devices := await self._get_home_devices(home_id)) is None:
+                continue
+            valid_home = True
+            devices_list.extend(devices)
+
+        return devices_list if valid_home else None
+
+    async def get_devices_dashboard(self) -> list[dict] | None:
+        """
+        Get a list of devices associated with the user's account.
+        Return information about the devices based on old API call
+        """
         dashboard = await self.get2("service/application/dashboard")
+        if not isinstance(dashboard, dict):
+            _LOGGER.warning(
+                "LG API return invalid devices information: '%s'", dashboard
+            )
+            return None
         if self._common_lang_pack_url is None:
-            self._common_lang_pack_url = dashboard.get("langPackCommonUri")
+            if _COMMON_LANG_URI_ID in dashboard:
+                self._common_lang_pack_url = dashboard[_COMMON_LANG_URI_ID]
+            else:
+                self._common_lang_pack_url = self._auth.gateway.core.lang_pack_url
         return as_list(dashboard.get("item", []))
 
     async def monitor_start(self, device_id):
@@ -1280,7 +1366,9 @@ class ClientAsync:
     async def _load_devices(self, force_update: bool = False):
         """Load dict with available devices."""
         if self._session and (self._devices is None or force_update):
-            new_devices = await self._session.get_devices()
+            if (new_devices := await self._session.get_devices()) is None:
+                self._devices = None
+                return
             if self.emulation:
                 # for debug
                 if emul_device := self._load_emul_devices():
