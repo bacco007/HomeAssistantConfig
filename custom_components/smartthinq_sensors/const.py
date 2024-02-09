9e1720dc8b639515bdcc22ff5c8c14d3d476e23b
Thomas Baxter <thomas@thomasbaxter.info>
2024-02-09 21:00:18 +1100

    Update Dep: custom_components/smartthinq_sensors via HACS

--- custom_components/smartthinq_sensors/const.py
+++ custom_components/smartthinq_sensors/const.py
@@ -1,13 +1,13 @@
 """Constants for LGE ThinQ custom component."""
 
-__version__ = "0.37.2"
+__version__ = "0.38.0"
 PROJECT_URL = "https://github.com/ollo69/ha-smartthinq-sensors/"
 ISSUE_URL = f"{PROJECT_URL}issues"
 
 DOMAIN = "smartthinq_sensors"
 
 MIN_HA_MAJ_VER = 2023
-MIN_HA_MIN_VER = 10
+MIN_HA_MIN_VER = 12
 __min_ha_version__ = f"{MIN_HA_MAJ_VER}.{MIN_HA_MIN_VER}.0"
 
 # general sensor attributes
