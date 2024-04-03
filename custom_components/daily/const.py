005c3ceab63e9875df53c734995c315c4204b4a8
Thomas Baxter <thomas@thomasbaxter.info>
2024-04-03 15:00:17 +1100

    Update Dep: custom_components/daily via HACS

--- custom_components/daily/const.py
+++ custom_components/daily/const.py
@@ -3,7 +3,7 @@
 DOMAIN = "daily"
 NAME = "Daily Sensor"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "2024.4.0"
+VERSION = "2024.4.1"
 
 ISSUE_URL = "https://github.com/jeroenterheerdt/HADailySensor/issues"
 
