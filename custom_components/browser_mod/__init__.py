45198490b14f97da259a3946be9e1c8648b73830
Thomas Baxter <thomas@thomasbaxter.info>
2024-08-01 21:00:19 +1000

    Update Dep: custom_components/browser_mod via HACS

--- custom_components/browser_mod/__init__.py
+++ custom_components/browser_mod/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import Platform
 
 from .store import BrowserModStore
 from .mod_view import async_setup_view
@@ -8,6 +10,16 @@ from .service import async_setup_services
 
 _LOGGER = logging.getLogger(__name__)
 
+
+CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
+
+PLATFORMS = [
+    Platform.SENSOR,
+    Platform.BINARY_SENSOR,
+    Platform.LIGHT,
+    Platform.MEDIA_PLAYER,
+    Platform.CAMERA
+]
 
 async def async_setup(hass, config):
 
@@ -25,10 +37,7 @@ async def async_setup(hass, config):
 
 async def async_setup_entry(hass, config_entry):
 
-    for domain in ["sensor", "binary_sensor", "light", "media_player", "camera"]:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, domain)
-        )
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     await async_setup_connection(hass)
     await async_setup_view(hass)
