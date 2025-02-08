f7291bacc8fb36ad1cd3fea88e72c26237ad5b66
Thomas Baxter <thomas@thomasbaxter.info>
2025-02-08 12:00:27 +1100

    Update Dep: custom_components/unifi_mqtt via HACS

--- custom_components/unifi_mqtt/__init__.py
+++ custom_components/unifi_mqtt/__init__.py
@@ -76,7 +76,6 @@ async def async_setup_entry(hass, entry):
         for device in unifi_devices:
             # If the device is not a dictionary, log its value and skip processing it.
             if not isinstance(device, dict):
-                _LOGGER.error("Received device that is not a dictionary: %s", device)
                 continue
                 
             if not device.get("adopted"):
