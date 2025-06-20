8d5524bdceba7fd73edbc32687f61314e011e5cb
Thomas Baxter <thomas@thomasbaxter.info>
2025-06-20 12:00:26 +1000

    Update Dep: custom_components/material_symbols via HACS

--- custom_components/material_symbols/__init__.py
+++ custom_components/material_symbols/__init__.py
@@ -11,7 +11,7 @@ LOGGER = logging.getLogger(__name__)
 
 NAME = "Material Symbols"
 DOMAIN = "material_symbols"
-VERSION = "2025.05.18"
+VERSION = "2025.06.19"
 
 ICONS_URL = f"/{DOMAIN}"
 LOADER_URL = f"/{DOMAIN}/material_symbols.js"
