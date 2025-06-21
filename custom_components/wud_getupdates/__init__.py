9cd6eab4f14be30e0afe306e86f3c0a7d7b9e610
Thomas Baxter <thomas@thomasbaxter.info>
2025-06-21 12:00:25 +1000

    Update Dep: custom_components/wud_getupdates via HACS

--- custom_components/wud_getupdates/__init__.py
+++ custom_components/wud_getupdates/__init__.py
@@ -8,10 +8,10 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    await hass.config_entries.async_forward_entry_unload(entry, ["sensor"])
     return True
