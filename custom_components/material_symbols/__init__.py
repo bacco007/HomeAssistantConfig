1c61b42aa8ff1cc0933c9c8a39e4417602370cbb
Thomas Baxter <thomas@thomasbaxter.info>
2025-06-30 18:00:28 +1000

    Update Dep: custom_components/material_symbols via HACS

--- custom_components/material_symbols/__init__.py
+++ custom_components/material_symbols/__init__.py
@@ -11,7 +11,7 @@ LOGGER = logging.getLogger(__name__)
 
 NAME = "Material Symbols"
 DOMAIN = "material_symbols"
-VERSION = "2025.06.19"
+VERSION = "2025.06.24"
 
 ICONS_URL = f"/{DOMAIN}"
 LOADER_URL = f"/{DOMAIN}/material_symbols.js"
--- custom_components/material_symbols/material_symbols.js
+++ custom_components/material_symbols/material_symbols.js
@@ -106,7 +106,7 @@
 
     // Log a styled message to the console
     console.info(
-        "%c MATERIAL SYMBOLS                    %c %c 2025.06.19  ",
+        "%c MATERIAL SYMBOLS                    %c %c 2025.06.24  ",
         '@import url("https://fonts.googleapis.com/css2?family=Roboto");background-color:#1FBEF2;color:#FFFFFF;padding:3px 43px 2px 8px;border-radius:999vh;border:5px solid #1FBEF2;font-family:"Roboto", sans-serif;margin-top:18px',
         '@import url("https://fonts.googleapis.com/css2?family=Roboto");background-color:#FFFFFF;color:#1FBEF2;padding:3px 8px 2px 0;border-radius:999vh 0 0 999vh;border:0;font-family:"Roboto", sans-serif;margin-left:-94px',
         '@import url("https://fonts.googleapis.com/css2?family=Roboto");background-color:#FFFFFF;color:#1FBEF2;padding:3px 9px 2px 0;border-radius:0 999vh 999vh 0;border:0;font-family:"Roboto", sans-serif;margin-left:-1px;margin-bottom:18px'
